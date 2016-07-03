e4077357fc6ae915a803dc46e93c9b91bdfd3696
Harish.K <harish2704@gmail.com>
2016-07-04 01:05:10 +0530

    Update routes

--- routes.js
+++ routes.js
@@ -1,7 +1,19 @@
 
 var controllers = {
   auth: require( 'controllers/auth'),
+  users: require( 'controllers/users'),
 };
+
+
+function genHandler( fn, dataKey ){
+  return function( req, res, next ){
+    return fn( req[dataKey], req )
+      .then(function( data ){
+        res.sendSuccess(data);
+      })
+      .catch(next);
+  }
+}
 
 function mountAction( app, controllerName, actionName, controller ){
   var fn = controller[actionName];
@@ -10,7 +22,24 @@ function mountAction( app, controllerName, actionName, controller ){
   actionName = actionName.match( /(get|post|put|delete|patch)(.*)/ );
   method = actionName[1];
   urlPath = actionName[2];
-  app[method]( urlPath, fn );
+
+  switch( method ){
+    case 'get':
+      app.get( urlPath, genHandler(fn, 'query' ) );
+      break;
+    case 'post':
+      app.get( urlPath, genHandler(fn, 'body' ) );
+      break;
+    case 'put':
+      app.get( urlPath, genHandler(fn, 'body' ) );
+      break;
+    case 'delete':
+      app.get( urlPath, genHandler(fn, 'query' ) );
+      break;
+    case 'patch':
+      app.get( urlPath, genHandler(fn, 'body' ) );
+      break;
+  }
 }
 
 
